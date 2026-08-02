970d2bbc083c121a4b4445f7400ff270f2292c04
AN-maz <andrianian398@gmail.com>
2026-08-02 18:06:55 +0700

    config database neon postgre

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,21 +1,32 @@
-const { Pool } = require('pg')
+// const { Pool } = require('pg')
+
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
+// pool.connect((err,client,release) => {
+//     if(err){
+//         console.log("Gagal terhubung ke database postgreSQL :", err.stack);
+//     }else{
+//         console.log('Berhasil terhubung ke database postgreSQL');
+//     }
+
+//     if(client) release()
+// })
+
+// module.exports = pool;
+
+const {Pool} = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
-
-pool.connect((err,client,release) => {
-    if(err){
-        console.log("Gagal terhubung ke database postgreSQL :", err.stack);
-    }else{
-        console.log('Berhasil terhubung ke database postgreSQL');
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
     }
-
-    if(client) release()
 })
 
 module.exports = pool;
